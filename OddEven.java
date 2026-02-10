04624094b027aab3fba02c9ca6c3bf582059c0d3
mstlaboniakter1433-wq <mstlaboniakter1433@gmail.com>
2026-02-10 20:33:23 +0600

    Add files via upload

---
+++ OddEven.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+public class OddEven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter The Number x : "); 
+		int x = scanner.nextInt(); 
+		if(x%2==0) 
+		{ 
+			System.out.println("The number is even number"); 
+		} 
+		else 
+		{ 
+			System.out.println("The number is odd number"); 
+		} 
+		}
+
+	}
+
+
