c2b33b10072c31e4e04fb2f0379f9b8154bfd3ec
mstlaboniakter1433-wq <mstlaboniakter1433@gmail.com>
2026-02-27 14:06:40 +0600

    Add files via upload

---
+++ main3.java
@@ -0,0 +1,10 @@
+public class main3 {
+
+    static void mymethod(){
+        System.out.println("Im static");
+    }
+
+    public static void main(String[] args) {
+        main3.mymethod();
+    }
+}
