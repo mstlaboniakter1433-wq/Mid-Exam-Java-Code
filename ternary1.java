9e24d4df55b4a35ae8607e43f17e7dc406283503
mstlaboniakter1433-wq <mstlaboniakter1433@gmail.com>
2026-04-01 17:19:15 +0600

    Add files via upload

---
+++ ternary1.java
@@ -0,0 +1,17 @@
+public class ternary1 {
+    public static void main(String[] args) {
+        int a=10,b=15;
+
+        int max = (a>b) ? a:b;
+
+
+        // if(a>b){
+        //     max=a;
+        // }else {
+        //     max=b;
+        // }
+
+        
+        System.out.print(max);
+    }
+}
