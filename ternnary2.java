307ddcff10eaf13154ee5b4b44d60e9808246957
mstlaboniakter1433-wq <mstlaboniakter1433@gmail.com>
2026-04-04 20:31:27 +0600

    Add files via upload

---
+++ ternnary2.java
@@ -0,0 +1,11 @@
+public class ternnary2 {
+    
+     public static void main(String[] args) {
+    
+    int num=4;
+
+    String result= (num%2 == 0) ? "Even" : "Odd";
+
+    System.out.print(result);
+     }
+}
