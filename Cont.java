123c17bd9fdfb957515e57d791a1ce81fa347cc1
mstlaboniakter1433-wq <mstlaboniakter1433@gmail.com>
2026-04-16 22:57:58 +0600

    Add files via upload

---
+++ Cont.java
@@ -0,0 +1,10 @@
+public class Cont {
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++){
+            if(i>=4 && i<=7){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
