9d00bf8e79d503f8a1c82ac8c43ceff558b06b8e
mstlaboniakter1433-wq <mstlaboniakter1433@gmail.com>
2026-04-20 23:26:46 +0600

    Add files via upload

---
+++ main5.java
@@ -0,0 +1,23 @@
+public class main5 {
+    public static void main(String[] args) {
+        int num = 2;
+
+        switch (num){
+            case 0:
+            System.out.println("number is 0");
+            
+            case 1:
+            System.out.println("number is 1");
+            
+            case 2:
+            System.out.println("number is 2");
+            break;
+
+            case 3:
+            System.out.println("number is 3");
+
+            default:
+            System.out.println(1);
+        }
+    }
+}
