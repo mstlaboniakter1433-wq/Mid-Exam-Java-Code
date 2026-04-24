72be5bfbaa52561287f5bf9490f519c499b5e889
mstlaboniakter1433-wq <mstlaboniakter1433@gmail.com>
2026-04-24 20:24:57 +0600

    Add files via upload

---
+++ DefaultConstructor1.java
@@ -0,0 +1,9 @@
+class DefaultConstructor1 {
+    DefaultConstructor1() {
+        System.out.println("This Constructor is DefaultConstructor");
+    }
+
+    public static void main(String[] args) {
+        DefaultConstructor1 d1 = new  DefaultConstructor1(); 
+    }
+}
