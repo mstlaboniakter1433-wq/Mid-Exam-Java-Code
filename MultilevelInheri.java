a147956ff0f7de5a8d680ac87b26783746568e7f
mstlaboniakter1433-wq <mstlaboniakter1433@gmail.com>
2026-04-26 22:32:54 +0600

    Add files via upload

---
+++ MultilevelInheri.java
@@ -0,0 +1,28 @@
+class Animal {
+    void eat() {
+        System.out.println("Eating");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Barking");
+    }
+}
+
+class BabyDog extends Dog {
+    void weep() {
+        System.out.println("Weeping");
+    }
+}
+
+public class MultilevelInheri {
+    public static void main(String[] args) {
+        BabyDog d = new BabyDog();
+
+        d.eat();   
+        d.bark(); 
+        d.weep();  
+    }
+    
+}
